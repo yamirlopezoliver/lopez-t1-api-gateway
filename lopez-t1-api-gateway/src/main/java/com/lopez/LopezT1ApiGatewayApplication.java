ebf8e6b40abb0fa6b76d4ac94315057849376716
Yamir <yamirlopez24@gmail.com>
2025-06-08 22:31:14 -0500

    Initial commit

---
+++ lopez-t1-api-gateway/src/main/java/com/lopez/LopezT1ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.lopez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LopezT1ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LopezT1ApiGatewayApplication.class, args);
+	}
+
+}
